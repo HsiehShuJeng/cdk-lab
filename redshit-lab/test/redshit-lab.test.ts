4203b653b7a44c5c8b797736363470064087f7ae
scott.hsieh[謝書正] <scott.hsieh@scotthsieh-mac13.local>
2022-06-11 01:02:59 +0800

    initialize Redshift lab

---
+++ redshit-lab/test/redshit-lab.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as RedshitLab from '../lib/redshit-lab-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/redshit-lab-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RedshitLab.RedshitLabStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
