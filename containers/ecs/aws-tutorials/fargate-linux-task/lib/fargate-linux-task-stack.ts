1511468721b9fac10abcfd8b3c238384c6db5c12
scott.hsieh[謝書正] <scott.hsieh@scotthsieh-mac13.local>
2022-08-16 00:56:33 +0800

    [fargate-linux-task] add task definition

--- containers/ecs/aws-tutorials/fargate-linux-task/lib/fargate-linux-task-stack.ts
+++ containers/ecs/aws-tutorials/fargate-linux-task/lib/fargate-linux-task-stack.ts
@@ -11,5 +11,27 @@ export class FargateLinuxTaskStack extends cdk.Stack {
       clusterName: 'fargate-cluster',
       vpc: ec2.Vpc.fromLookup(this, 'defaultVpc', { isDefault: true })
     });
+
+    const fargateTaskDefinition = new ecs.FargateTaskDefinition(this, 'LinuxTaskDefinition', {
+      family: 'sample-fargate',
+      memoryLimitMiB: 512,
+      cpu: 256,
+    });
+    fargateTaskDefinition.addContainer("Container", {
+      containerName: 'fargate-app',
+      image: ecs.ContainerImage.fromRegistry("public.ecr.aws/docker/library/httpd:latest"),
+      portMappings: [{
+        containerPort: 80,
+        hostPort: 80,
+        protocol: ecs.Protocol.TCP
+      }],
+      essential: true,
+      entryPoint: [
+        'sh', '-c'
+      ],
+      command: [
+        "/bin/sh -c \"echo '<html> <head> <title>Amazon ECS Sample App</title> <style>body {margin-top: 40px; background-color: #333;} </style> </head><body> <div style=color:white;text-align:center> <h1>Amazon ECS Sample App</h1> <h2>Congratulations!</h2> <p>Your application is now running on a container in Amazon ECS.</p> </div></body></html>' >  /usr/local/apache2/htdocs/index.html && httpd-foreground\""
+      ]
+    });
   }
 }
